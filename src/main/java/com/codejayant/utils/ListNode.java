eb359c8af9cf5367d067bb5356187094de14a081
jayant sarkar <codejayant@gmail.com>
2020-02-20 21:36:06 -0800

    object comparison equals and hashCode manipulation example

--- src/main/java/com/codejayant/utils/ListNode.java
+++ src/main/java/com/codejayant/utils/ListNode.java
@@ -8,4 +8,24 @@ public class ListNode {
     public ListNode(int x) {
         this.val = x;
     }
+
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (!(obj instanceof ListNode)) {
+//            return false;
+//        }
+//
+//        if (this == obj) {
+//            return true;
+//        }
+//
+//        return ((ListNode) obj).val == this.val;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return this.val;
+//    }
+
 }
