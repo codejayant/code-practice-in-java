819be93574b69c87d369584602fcd4a1380682e4
jayant sarkar <codejayant@gmail.com>
2020-02-29 20:19:27 -0800

    frequency sort using bucket sort

--- src/main/java/com/codejayant/strings/FrequencySort.java
+++ src/main/java/com/codejayant/strings/FrequencySort.java
@@ -48,6 +48,10 @@ public class FrequencySort {
         System.out.println(frequencySort("tree"));
         System.out.println(frequencySort("Aabb"));
         System.out.println(frequencySort("cccaaa"));
+
+        System.out.println(frequencySort2("tree"));
+        System.out.println(frequencySort2("Aabb"));
+        System.out.println(frequencySort2("cccaaa"));
     }
 
     /**
@@ -80,6 +84,47 @@ public class FrequencySort {
             }
         }
 
+        return sb.toString();
+    }
+
+    /**
+     * T: O(n)
+     * @param s
+     * @return
+     */
+    private static String frequencySort2(String s) {
+
+
+        if (s == null || s.isEmpty()) return s;
+
+        // Count up the occurrences
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        int maximumFrequency = Collections.max(counts.values());
+
+        // Make the list of buckets and apply bucket sort.
+        // bucket sort makes sorting T: O(n)
+        List<List<Character>> buckets = new ArrayList<>();
+        for (int i = 0; i <= maximumFrequency; i++) {
+            buckets.add(new ArrayList<>());
+        }
+        for (Character key : counts.keySet()) {
+            int freq = counts.get(key);
+            buckets.get(freq).add(key);
+        }
+
+        // Build up the string.
+        StringBuilder sb = new StringBuilder();
+        for (int i = buckets.size() - 1; i >= 1; i--) {
+            for (Character c : buckets.get(i)) {
+                for (int j = 0; j < i; j++) {
+                    sb.append(c);
+                }
+            }
+        }
         return sb.toString();
     }
 
