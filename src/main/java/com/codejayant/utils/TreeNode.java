da215db47f24fd14f9a7a52f2aa0193ef40a8778
jayant sarkar <codejayant@gmail.com>
2020-04-02 01:27:13 -0700

    connect level order siblings

--- src/main/java/com/codejayant/utils/TreeNode.java
+++ src/main/java/com/codejayant/utils/TreeNode.java
@@ -5,6 +5,7 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
+    public TreeNode next;
 
     public TreeNode(int val) {
         this.val = val;
@@ -26,6 +27,14 @@ public class TreeNode {
         this.right = right;
     }
 
+    public TreeNode getNext() {
+        return next;
+    }
+
+    public void setNext(TreeNode next) {
+        this.next = next;
+    }
+
     public int getVal() {
         return val;
     }
@@ -34,8 +43,29 @@ public class TreeNode {
         this.val = val;
     }
 
+    // level order traversal using 'next' pointer
+    public void printLevelOrder() {
+        TreeNode nextLevelRoot = this;
+        while (nextLevelRoot != null) {
+            TreeNode current = nextLevelRoot;
+            nextLevelRoot = null;
+            while (current != null) {
+                System.out.print(current.val + " ");
+                if (nextLevelRoot == null) {
+                    if (current.left != null)
+                        nextLevelRoot = current.left;
+                    else if (current.right != null)
+                        nextLevelRoot = current.right;
+                }
+                current = current.getNext();
+            }
+            System.out.println();
+        }
+    }
+
     @Override
     public String toString() {
         return String.valueOf(val);
     }
+
 }
